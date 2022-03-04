64f147b3f0bc9d1885bf9438c748d1c2617a8166
신용석 <help@studio-thinkers.com>
2022-03-04 19:29:39 +0900

    commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = 5001
+const cors = require('cors')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
